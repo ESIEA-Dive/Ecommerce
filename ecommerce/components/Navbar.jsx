d0ad7c64b2a29626235a1331bbf6879fa82fb6bf
unknown <dive@et.esiea.fr>
2022-10-28 00:51:24 +0200

    Product Details

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Link from 'next/Link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>Onepoint Fake Shop</Link>
+      </p>
+
+      <button type='button' className='cart-icon'>
+        <AiOutlineShopping />
+        <span className='cart-item-qty'></span>
+      </button>
+    </div>
   )
 };
 
